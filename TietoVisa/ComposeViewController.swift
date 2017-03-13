4104360d1b7277e6bcc4f90b1d19d0a66707718b
Marko Lehtola <marko@Marko-MacBook-Pro.local>
2017-03-13 23:24:31 +0200

    lisättiin UI

---
+++ TietoVisa/ComposeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ComposeViewController.swift
+//  TietoVisa
+//
+//  Created by Marko Lehtola on 13.3.2017.
+//  Copyright © 2017 Marko Lehtola. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+        override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func addPost(_ sender: Any) {
+        //post data to firebase
+        
+        //dismiss the pop over
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+
+
+    @IBAction func cancelPost(_ sender: Any) {
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+}
