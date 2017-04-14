808a8e547b98ef828a420963092c7e0f9b231ad4
Marko Lehtola <marko@marko-macbook-pro.local>
2017-04-14 13:38:22 +0300

    oikean vastauksen näyttö ikonilla toimii!

--- TietoVisa/GameScreenViewController.swift
+++ TietoVisa/GameScreenViewController.swift
@@ -23,6 +23,9 @@ class GameScreenViewController: UIViewController  {
     var categoryName:String = ""
     var numberOfQuestion:Int = 0
     var ref:FIRDatabaseReference?
+    var outOfThree = 0
+    var rightAnswer = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -78,11 +81,50 @@ class GameScreenViewController: UIViewController  {
             let vaara2Vastaus = value?["answer3"] as? String ?? ""
             let vaara3Vastaus = value?["answer4"] as? String ?? ""
             
-            self.kysymysLabel.text = kysymys
-            self.vastaus1Button.setTitle(oikeaVastaus, for: .init())
-            self.vastaus2Button.setTitle(vaara1Vastaus, for: .init())
-            self.vastaus3Button.setTitle(vaara2Vastaus, for: .init())
-            self.vastaus4Button.setTitle(vaara3Vastaus, for: .init())
+            self.rightAnswer = Int(arc4random_uniform(4)) + 1
+            
+            if self.rightAnswer == 1{
+                
+                self.kysymysLabel.text = kysymys
+                self.vastaus1Button.setTitle(oikeaVastaus, for: .init())
+                self.vastaus2Button.setTitle(vaara1Vastaus, for: .init())
+                self.vastaus3Button.setTitle(vaara2Vastaus, for: .init())
+                self.vastaus4Button.setTitle(vaara3Vastaus, for: .init())
+                
+            }
+            
+            if self.rightAnswer == 2{
+                
+                self.kysymysLabel.text = kysymys
+                self.vastaus1Button.setTitle(vaara1Vastaus, for: .init())
+                self.vastaus2Button.setTitle(oikeaVastaus, for: .init())
+                self.vastaus3Button.setTitle(vaara2Vastaus, for: .init())
+                self.vastaus4Button.setTitle(vaara3Vastaus, for: .init())
+                
+            }
+            
+            if self.rightAnswer == 3{
+                
+                self.kysymysLabel.text = kysymys
+                self.vastaus1Button.setTitle(vaara1Vastaus, for: .init())
+                self.vastaus2Button.setTitle(vaara2Vastaus, for: .init())
+                self.vastaus3Button.setTitle(oikeaVastaus, for: .init())
+                self.vastaus4Button.setTitle(vaara3Vastaus, for: .init())
+                
+            }
+            
+            if self.rightAnswer == 4{
+                
+                self.kysymysLabel.text = kysymys
+                self.vastaus1Button.setTitle(vaara1Vastaus, for: .init())
+                self.vastaus2Button.setTitle(vaara2Vastaus, for: .init())
+                self.vastaus3Button.setTitle(vaara3Vastaus, for: .init())
+                self.vastaus4Button.setTitle(oikeaVastaus, for: .init())
+                
+            }
+            
+            
+            
             
             // ...
         }) { (error) in
@@ -91,13 +133,44 @@ class GameScreenViewController: UIViewController  {
     }
   
     @IBAction func vastaus1ButtonAction(_ sender: UIButton) {
+        //Lisätään kysyttyjen kysymysten countteriin yksi
+        outOfThree += 1
+        //tarkistetaan onko tämä nappula oikea vastaus
+        if rightAnswer == 1 {
+            vastaus1Button.setBackgroundImage(#imageLiteral(resourceName: "yesIcon"), for: .normal)
+        }
+        else{
+            vastaus1Button.setBackgroundImage(#imageLiteral(resourceName: "noIcon"), for: .normal)
+        }
+        
         
     }
     @IBAction func vastaus2ButtonAction(_ sender: UIButton) {
+        outOfThree += 1
+        if rightAnswer == 2 {
+            vastaus2Button.setBackgroundImage(#imageLiteral(resourceName: "yesIcon"), for: .normal)
+        }
+        else{
+            vastaus2Button.setBackgroundImage(#imageLiteral(resourceName: "noIcon"), for: .normal)
+        }
     }
     @IBAction func vastaus3ButtonAction(_ sender: UIButton) {
+        outOfThree += 1
+        if rightAnswer == 3 {
+            vastaus3Button.setBackgroundImage(#imageLiteral(resourceName: "yesIcon"), for: .normal)
+        }
+        else{
+            vastaus3Button.setBackgroundImage(#imageLiteral(resourceName: "noIcon"), for: .normal)
+        }
     }
     @IBAction func vastaus4ButtonAction(_ sender: UIButton) {
+        outOfThree += 1
+        if rightAnswer == 4 {
+            vastaus4Button.setBackgroundImage(#imageLiteral(resourceName: "yesIcon"), for: .normal)
+        }
+        else{
+            vastaus4Button.setBackgroundImage(#imageLiteral(resourceName: "noIcon"), for: .normal)
+        }
     }
     
 }
